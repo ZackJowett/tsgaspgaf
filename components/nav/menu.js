57e6556684c32fd3fc919404fb1420e78a5338c7
zackjowett <zjwebdev@gmail.com>
2023-04-21 18:10:48 +1000

    moved a inside h2

--- components/nav/menu.js
+++ components/nav/menu.js
@@ -1,18 +1,18 @@
 export default function Menu() {
 	return (
 		<nav className="absolute top-1/4 main-nav flex flex-col justify-start h-full text-left">
-			<a className="mb-2 hover:font-medium">
-				<h2 className="">STORY</h2>
-			</a>
-			<a className="mb-2 hover:font-medium">
-				<h2>CREW</h2>
-			</a>
-			<a className="mb-2 hover:font-medium">
-				<h2>DONATE</h2>
-			</a>
-			<a className="hover:font-medium">
-				<h2>CONTACT</h2>
-			</a>
+			<h2 className="mb-2">
+				<a>STORY </a>
+			</h2>
+			<h2 className="mb-2">
+				<a>CREW </a>
+			</h2>
+			<h2 className="mb-2">
+				<a>DONATE </a>
+			</h2>
+			<h2 className="">
+				<a>CONTACT</a>
+			</h2>
 		</nav>
 	);
 }
